592cb9f3b6b4b233088f765deaa9c2b7324f0ec8
bavshehata <bavshehata@gmail.com>
2022-01-08 17:36:04 +0200

    Adding a root query

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 
 app.listen(4000, () => {
   console.log("Listening for requests on port 4000");
 });
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+  })
+);
